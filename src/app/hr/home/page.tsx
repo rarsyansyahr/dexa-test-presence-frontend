6673271aef256be75f0d9715ae90013ff7e75495
Arsyansyah <rarsyansyahr@gmail.com>
2024-04-23 11:21:31 +0700

    create: base hr home page

--- src/app/hr/home/page.tsx
+++ src/app/hr/home/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
 import React, { FC } from "react";
 
 const HomePage: FC = () => {
-  return <div>Hai, Sdr. Rizky</div>;
+  return (
+    <div className="flex justify-center items-center">
+      <Image
+        src="/images/logo.png"
+        width={800}
+        height={400}
+        alt="Logo"
+        className="w-56 md:w-80"
+      />
+      {/* <div>Hai, Sdr. Rizky</div> */}
+    </div>
+  );
 };
 
 export default HomePage;
